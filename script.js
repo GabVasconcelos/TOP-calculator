b7590242eb400e2c0e04362bbe0eee68f67fe7b4
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-04-14 14:44:20 -0300

    Add main script functionality (this is a bad habit)

---
+++ script.js
@@ -0,0 +1,84 @@
+const buttons = document.querySelectorAll(".buttons button")
+const functions = document.querySelectorAll(".functions button")
+const result = document.querySelector(".result")
+const equal = document.querySelector(".equal")
+
+let numbers = {
+    first: "",
+    second: "",
+    operation: "",
+    current: "first",
+    reset: false
+}
+
+const operations = {
+    "+": (one, two) => one + two,
+    "-": (one, two) => one - two,
+    "*": (one, two) => one * two,
+    "/": (one, two) => one / two,
+}
+
+function clear() {
+    numbers.first = ""
+    numbers.second = ""
+    numbers.operation = ""
+    numbers.current = "first"
+    updateTextBox()
+}
+
+function updateTextBox(text) {
+    result.textContent = text || `${numbers.first} ${numbers.operation} ${numbers.second}`
+}
+
+function appendNumber(number) {
+    numbers[numbers.current] = numbers.reset && String(number) || Number(String(numbers[numbers.current] + number))
+    numbers.reset = false
+    updateTextBox()
+}
+
+function setOperation(operation) {
+    if (numbers.first == "") {
+        return
+    }
+    if (numbers.reset) {
+        numbers.second = ""
+        numbers.reset = false
+    }
+    numbers.operation = operation
+    numbers.current = "second"
+    updateTextBox()
+}
+
+function processButtonPress(button, type) {
+    let f = type == "number" && appendNumber || setOperation
+    f(button.value)
+}
+
+buttons.forEach(button => {
+    button.addEventListener("click", event => {
+        processButtonPress(event.target, "number")
+    })
+})
+
+functions.forEach(button => {
+    button.addEventListener("click", event => {
+        if (event.target.value == "C") {
+            clear() 
+            return
+        }
+        processButtonPress(event.target, "function")
+    })
+})
+
+equal.addEventListener("click", event => {
+    if (numbers.first == "" || numbers.second == "" || numbers.operation == "") {
+        return
+    }
+    let result = operations[numbers.operation](Number(numbers.first), Number(numbers.second))
+    updateTextBox(result)
+    numbers.first = String(result)
+    numbers.reset = true
+    // numbers.second = ""
+    // numbers.operation = ""
+    numbers.current = "second"
+})
